41a89f5e5de91205ff6cd43aa4a297989316424c
Pumpkin-spice-cloud <alan_mo@live.com>
2020-02-12 20:13:36 -0500

    adds spec test for checking if cart increments when product added to cart

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+  
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )     
+    end
+  end
+  scenario "Cart increases by one when a product is added to cart" do
+    visit root_path
+    within first('.product') do 
+      click_on 'Add'
+    end
+    expect(page).to have_content('(1)')
+  end
+end
+
